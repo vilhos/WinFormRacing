19390dce832d83477d842c9ab17345870cde93fd
Vilen <abyancz@gmail.com>
2021-11-14 23:20:35 +0400

    Add project files.

---
+++ WinFormRacing/Race.Designer.cs
@@ -0,0 +1,121 @@
+﻿
+using System.Windows.Forms;
+
+namespace WinFormRacing
+{
+    partial class Race
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        private void InitializeComponent()
+        {
+            this.car1 = new System.Windows.Forms.Button();
+            this.car2 = new System.Windows.Forms.Button();
+            this.car3 = new System.Windows.Forms.Button();
+            this.start = new System.Windows.Forms.Button();
+            this.track = new System.Windows.Forms.PictureBox();
+            this.finish = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.track)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.finish)).BeginInit();
+            this.SuspendLayout();
+            Control.CheckForIllegalCrossThreadCalls = false;
+            // 
+            // car1
+            // 
+            this.car1.BackColor = System.Drawing.SystemColors.Highlight;
+            this.car1.Location = new System.Drawing.Point(48, 51);
+            this.car1.Name = "car1";
+            this.car1.Size = new System.Drawing.Size(101, 37);
+            this.car1.TabIndex = 0;
+            this.car1.Text = "car1";
+            this.car1.UseVisualStyleBackColor = false;
+            // 
+            // car2
+            // 
+            this.car2.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.car2.Location = new System.Drawing.Point(48, 138);
+            this.car2.Name = "car2";
+            this.car2.Size = new System.Drawing.Size(101, 37);
+            this.car2.TabIndex = 1;
+            this.car2.Text = "car2";
+            this.car2.UseVisualStyleBackColor = false;
+            // 
+            // car3
+            // 
+            this.car3.BackColor = System.Drawing.SystemColors.Info;
+            this.car3.Location = new System.Drawing.Point(48, 227);
+            this.car3.Name = "car3";
+            this.car3.Size = new System.Drawing.Size(101, 37);
+            this.car3.TabIndex = 2;
+            this.car3.Text = "car3";
+            this.car3.UseVisualStyleBackColor = false;
+            // 
+            // start
+            // 
+            this.start.Location = new System.Drawing.Point(326, 320);
+            this.start.Name = "start";
+            this.start.Size = new System.Drawing.Size(150, 49);
+            this.start.TabIndex = 3;
+            this.start.Text = "start";
+            this.start.UseVisualStyleBackColor = true;
+            this.start.Click += new System.EventHandler(this.StartClick);
+            // 
+            // pictureBox1
+            // 
+            this.track.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.track.Location = new System.Drawing.Point(48, 51);
+            this.track.Name = "track";
+            this.track.Size = new System.Drawing.Size(710, 213);
+            this.track.TabIndex = 4;
+            this.track.TabStop = false;
+
+            // 
+            // finish
+            // 
+            this.finish.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.finish.Location = new System.Drawing.Point(727, 51);
+            this.finish.Name = "finish";
+            this.finish.Size = new System.Drawing.Size(31, 213);
+            this.finish.TabIndex = 5;
+            this.finish.TabStop = false;
+            // 
+            // Race
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 417);
+            this.Controls.Add(this.finish);
+            this.Controls.Add(this.start);
+            this.Controls.Add(this.car3);
+            this.Controls.Add(this.car2);
+            this.Controls.Add(this.car1);
+            this.Controls.Add(this.track);
+            this.Name = "Race";
+            this.Text = "Race";
+            ((System.ComponentModel.ISupportInitialize)(this.track)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.finish)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button car1;
+        private System.Windows.Forms.Button car2;
+        private System.Windows.Forms.Button car3;
+        private System.Windows.Forms.Button start;
+        private System.Windows.Forms.PictureBox track;
+        private System.Windows.Forms.PictureBox finish;
+    }
+}
---
+++ WinFormRacing/Race.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Drawing;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace WinFormRacing
+{
+    public partial class Race : Form
+    {
+        bool car_1 = false;
+        bool car_2 = false;
+        bool car_3 = false;
+        Random random = new Random();
+        public Race()
+        {
+            InitializeComponent();
+        }
+        private void StartClick(object sender, EventArgs e)
+        {
+            Thread thread1 = new Thread(() => MoveCar1(car1));
+            Thread thread2 = new Thread(() => MoveCar2(car2));
+            Thread thread3 = new Thread(() => MoveCar3(car3));
+
+            thread1.Start();
+            thread2.Start();
+            thread3.Start();
+        }
+
+        public void MoveCar1(Button car)
+        {
+            While(car);
+            car_1 = true;
+
+            if (car_2 == false && car_3 == false)
+                MessageBox.Show($"{car1.Name} is win");
+        }
+
+        public void MoveCar2(Button car)
+        {
+            While(car);
+            car_2 = true;
+            if (car_1 == false && car_3 == false)
+                MessageBox.Show($"{car2.Name} is win");
+        }
+
+        public void MoveCar3(Button car)
+        {
+            While(car);
+            car_3 = true;
+            if (car_1 == false && car_2 == false)
+                MessageBox.Show($"{car3.Name} is win");
+        }
+
+        public void While(Button car)
+        {
+            while (car.Location.X <= finish.Location.X - car.Width)
+            {
+                car.Location = new Point(car.Location.X + random.Next(1, 10), car.Location.Y);
+                Thread.Sleep(random.Next(5, 50));
+            }
+        }
+    }
+}
+
+
